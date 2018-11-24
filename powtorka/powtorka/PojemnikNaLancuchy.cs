e2f133bfd8f466eb3fa68c4032e483230c1b4d27
MartykQ <fmartyka16@gmail.com>
2018-11-24 19:00:02 +0100

    serial
    serial

--- powtorka/powtorka/PojemnikNaLancuchy.cs
+++ powtorka/powtorka/PojemnikNaLancuchy.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace powtorka
+
 {
-    class PojemnikNaLancuchy
+    [Serializable]
+    public class PojemnikNaLancuchy : ICloneable, IComparable, IEquatable<PojemnikNaLancuchy>
     {
         private string nazwaLancucha;
         private ArrayList kolekcja;
@@ -29,7 +34,7 @@ namespace powtorka
 
         }
 
-        public PojemnikNaLancuchy(string naz, string sr): this(naz)
+        public PojemnikNaLancuchy(string naz, string sr) : this(naz)
         {
             this.DodajString(sr);
         }
@@ -45,7 +50,7 @@ namespace powtorka
             {
                 kolekcja.Insert(i, s);
             }
-            catch(IndexOutOfRangeException) //ktory
+            catch (IndexOutOfRangeException) //ktory
             {
                 Console.WriteLine("Zly indeks!");
             }
@@ -57,7 +62,7 @@ namespace powtorka
             {
                 kolekcja.RemoveAt(i);
             }
-            catch(IndexOutOfRangeException)
+            catch (IndexOutOfRangeException)
             {
                 Console.WriteLine("Zly indeks");
             }
@@ -66,30 +71,15 @@ namespace powtorka
 
         public void Zamien(string stary, string nowy)
         {
-            if (kolekcja.Contains(stary))
-            {
-                this.WstawIndeks(kolekcja.IndexOf(stary), nowy);
-                this.Usun(kolekcja.IndexOf(stary) + 1);
-                return;
-            }
-            else
-            {
-                Console.WriteLine("Nie ma takiego napisu!!");
-            }
 
+            kolekcja[kolekcja.IndexOf(stary)] = nowy;
 
         }
-            
-
-
-            
-
-        
 
         public override string ToString()
         {
             StringBuilder sb = new StringBuilder();
-            foreach(string s in kolekcja)
+            foreach (string s in kolekcja)
             {
                 sb.Append(s);
                 sb.Append(" ");
@@ -97,7 +87,85 @@ namespace powtorka
             return sb.ToString();
         }
 
+        public object Clone() //mialo byc binarnie...
+        {
+            /*
+            XmlSerializer xs = new XmlSerializer(typeof(PojemnikNaLancuchy));
+            StreamWriter sw = new StreamWriter("PojemnikNaLancuchy.xml");
+            xs.Serialize(sw, this);
+            sw.Close();
 
+            XmlSerializer dsr = new XmlSerializer(typeof(PojemnikNaLancuchy));
+            TextReader tr = new StreamReader("PojemnikNaLancuchy.xml");
+            PojemnikNaLancuchy nowyPojemnik;
+            nowyPojemnik = (PojemnikNaLancuchy)dsr.Deserialize(tr);
+            tr.Close();
+            return nowyPojemnik;
+            */
 
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream fs = new FileStream("deepCopy", FileMode.Create);
+            bf.Serialize(fs, this);
+            fs.Close();
+            FileStream rf = new FileStream("deepCopy", FileMode.Open);
+            PojemnikNaLancuchy nowyPojemnik = (PojemnikNaLancuchy)bf.Deserialize(rf);
+            return nowyPojemnik;
+        }
+
+        public object CloneSimple() //zwykly DeppClone
+        {
+            PojemnikNaLancuchy nowy = new PojemnikNaLancuchy();
+            nowy.NazwaLancucha = this.NazwaLancucha;
+            foreach(string s in this.kolekcja)
+            {
+                nowy.kolekcja.Add(s);
+            }
+            return nowy;
+        }
+
+        public int CompareTo(object obj)
+        {
+            int comparer;
+            PojemnikNaLancuchy nowy = (PojemnikNaLancuchy)obj;
+            for (int i = 0; i < Math.Min(this.kolekcja.Count, nowy.Kolekcja.Count); i++)
+            {
+                string s1 = (string)this.kolekcja[i];
+                string s2 = (string)nowy.kolekcja[i];
+                comparer = s1.CompareTo(s2);
+                if (comparer != 0) return comparer;
+
+            }
+            return 0;
+
+        }
+
+        public bool Equals(PojemnikNaLancuchy other)
+        {
+            if (other.NazwaLancucha.Equals(this.NazwaLancucha) && other.Kolekcja.Count == this.Kolekcja.Count)
+            {
+                for(int i = 0; i<this.Kolekcja.Count; i++)
+                {
+                    if (!other.Kolekcja[i].Equals(this.kolekcja[i])) return false;
+                }
+                return true;
+            }
+            else return false;
+        }
+
+        public void ZapiszXML(string nazwa)
+        {
+            XmlSerializer sr = new XmlSerializer(typeof(PojemnikNaLancuchy));
+            StreamWriter sw = new StreamWriter(nazwa);
+            sr.Serialize(sw, this);
+            sw.Close();
+
+        }
+
+        public static object WczytajXML(string nazwa)
+        {
+            XmlSerializer sr = new XmlSerializer(typeof(PojemnikNaLancuchy));
+            TextReader tr = new StreamReader(nazwa);
+            return (object)sr.Deserialize(tr);
+        }
     }
 }
--- powtorka/powtorka/Program.cs
+++ powtorka/powtorka/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,13 +13,49 @@ namespace powtorka
         {
 
             PojemnikNaLancuchy poj1 = new PojemnikNaLancuchy("nazwa", "pierwszy napis");
-            poj1.DodajString("drugi string");
-            poj1.WstawIndeks(0, "dodanyjakopierw");
-            poj1.Zamien("dodanyjakopierw", "zamieniony");
+            poj1.DodajString("jeden");
+            poj1.DodajString("dwa");
+            poj1.WstawIndeks(0, "naZero");
             
+            PojemnikNaLancuchy poj2 = (PojemnikNaLancuchy)poj1.Clone();
+            poj2.Zamien("dwa", "zamieniony");
+           
 
             Console.WriteLine(poj1);
+            Console.WriteLine("==============");
+            Console.WriteLine(poj2);
+            Console.WriteLine(poj1.Equals(poj2));
+            
+            PojemnikNaLancuchy p1 = new PojemnikNaLancuchy("p1", "a");
+            PojemnikNaLancuchy p2 = new PojemnikNaLancuchy("p2", "b");
+            PojemnikNaLancuchy p3 = new PojemnikNaLancuchy("p3", "c");
+            
+   
+          
+
+            
+            Console.WriteLine("xDxDxDxDxD");
+
+            ArrayList sortowalna = new ArrayList();
+            sortowalna.Add(p3);
+            sortowalna.Add(p2);
+            sortowalna.Add(p1);
+
+
+            sortowalna.Sort();
+            
+            foreach(PojemnikNaLancuchy p in sortowalna)
+            {
+                Console.WriteLine(p);
+            }
+
+
+            poj1.ZapiszXML("nowyPlik");
+            
+            PojemnikNaLancuchy poj3 = (PojemnikNaLancuchy)PojemnikNaLancuchy.WczytajXML("nowyPlik");
+            Console.WriteLine(poj3);
             Console.ReadKey();
+            
         }
     }
 }
